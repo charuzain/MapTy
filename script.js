1ea66b7a5ae57d1cb82333f66e7dce55bc17ab8e
charuzain <charuabhizain@gmail.com>
2023-03-20 21:40:16 -0400

    Displaying Map using leaflet library

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ window.addEventListener('DOMContentLoaded', function () {
   navigator.geolocation.getCurrentPosition(function (position) {
     const latitude = position.coords.latitude
     const longitude = position.coords.longitude
-    console.log(latitude , longitude)
-    
+    console.log(latitude, longitude)
+
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 18);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      //copyright info below the map
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    L.marker(coords)
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
   },
     function (error) {
   alert("there is some error")
